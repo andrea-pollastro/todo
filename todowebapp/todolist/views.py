a88d9b3544e4fb90dbf2699a5959d41dae160a27
Andrea Pollastro <23277156+andrea-pollastro@users.noreply.github.com>
2025-11-02 18:15:36 +0100

    small refactoring + tasks are returned ordered by priority (desc)

--- todowebapp/todolist/views.py
+++ todowebapp/todolist/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from .models import Task
 
-# Create your views here.
-def index(request):
+def get_all_tasks(request):
     context = {
         'fields': [f.name
                     .replace('_', ' ')
                     .capitalize() for f in Task._meta.get_fields() if f.name != 'id'],
-        'tasks': Task.objects.all()
+        'tasks': Task.objects.all().order_by('-priority')
     }
     return render(request, 'list-todo.html', context)
