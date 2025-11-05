b76528cd63b61cea38d4a790f9d44f67425d88c4
Andrea Pollastro <23277156+andrea-pollastro@users.noreply.github.com>
2025-11-05 21:49:15 +0100

    table style adjusted

--- todowebapp/todolist/views.py
+++ todowebapp/todolist/views.py
@@ -4,30 +4,11 @@ from django.utils import timezone
 from .models import Task
 from .forms import TaskForm
 
-# def task_list(request):
-#     tasks = Task.objects.all().order_by('-priority')
-#     form = TaskForm()
-
-#     if request.method == "POST":
-#         form = TaskForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('task_list')
-
-#     context = {
-#         'tasks': tasks,
-#         'fields': ['Status', 'Title', 'Priority', 'Due Date', 'Comment', 'Delivered To', 'Created', 'Updated'],
-#         'form': form,
-#     }
-#     return render(request, 'list_todo.html', context)
-
 def task_list(request):
-    # solo GET
     tasks = Task.objects.all().order_by('-priority')
     form = TaskForm()  # form vuoto per il modal
     context = {
         'tasks': tasks,
-        'fields': ['Status','Title','Priority','Due Date','Comment','Delivered To','Created','Updated'],
         'form': form,
         'today': timezone.localdate(),
     }
