1258139e7017cc0d2d154083b937927e42b38685
Andrea Pollastro <23277156+andrea-pollastro@users.noreply.github.com>
2025-11-05 23:41:13 +0100

    filtering on active only/all tasks added

--- todowebapp/todolist/views.py
+++ todowebapp/todolist/views.py
@@ -5,11 +5,19 @@ from .models import Task
 from .forms import TaskForm
 
 def task_list(request):
-    tasks = Task.objects.all().order_by('-priority')
-    form = TaskForm()  # form vuoto per il modal
+    # tasks = Task.objects.all().order_by('-priority')
+    if request.GET.get('show_completed') == '1':
+        tasks = Task.objects.all()
+        view = 'all'
+    else:
+        tasks = Task.objects.exclude(status=2)
+        view = 'active'
+    tasks = tasks.order_by('-priority')
+
     context = {
         'tasks': tasks,
-        'form': form,
+        'view': view,
+        'form': TaskForm(),
         'today': timezone.localdate(),
     }
     return render(request, 'list_todo.html', context)
