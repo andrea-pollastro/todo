c50d7ecb47c53edd05214278b94d1cd9731c1036
Andrea Pollastro <23277156+andrea-pollastro@users.noreply.github.com>
2025-11-06 18:50:14 +0100

    style changes (navbar on the left) + page hierarchy updated

--- todowebapp/todowebapp/settings.py
+++ todowebapp/todowebapp/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # for navbar.css
+    BASE_DIR / "todolist" / "static",        # for my-table.css
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
